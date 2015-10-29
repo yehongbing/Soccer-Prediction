7c05deeb290af0a10064954b863d2d75744161e4
yehongbing <hongbing.ye@outlook.com>
2015-10-28 22:33:41 -0400

    New data generated

--- Data_Processing/combine_by_id.py
+++ Data_Processing/combine_by_id.py
@@ -11,36 +11,68 @@ def combine_by_id(source_file, dest_file):
         writer = csv.writer(dest_file)
         desired_col = []
 
-        # before write to the row, we need to take the desired value from each row
+        result_temp = 0
+        row_num = 0
+
         for row in reader:
+
             if row_num == 0:
                 header = row
-                for i in range(0,7):
+                for i in range(0, 7):
                     desired_col.append(header[i])
-
                 desired_col.append(header[26])
                 desired_col.append(header[27])
+                desired_col.append("Final_Result")
                 writer.writerow(desired_col)
-            elif row_num == 1:
-                previous = row[0]
-                for i in range(0,7):
-                    desired_col.append(row[i])
-                desired_col.append(row[26])
-                desired_col.append(row[27])
-                writer.writerow(desired_col)
-                count += 1
-            else:
-                if row[0] != previous:
 
-                    previous = row[0]
-                    for i in range(0,7):
-                        desired_col.append(row[i])
-                    desired_col.append(row[26])
-                    desired_col.append(row[27])
+            elif row_num == 1:
+                previous = row
+                previous_matchID = previous[0]
+
+            else:
+                if row[0] != previous_matchID:
+                    for i in range(0, 7):
+                        desired_col.append(previous[i])
+                    desired_col.append(previous[26])
+                    desired_col.append(previous[27])
+                    # append the final result according to score difference
+                    if int(previous[11]) > 0:
+                        result_temp = 1
+                    elif int(previous[11]) == 0:
+                        result_temp = 0
+                    else:
+                        result_temp = -1
+
+                    # write the result to file
+                    desired_col.append(result_temp)
                     writer.writerow(desired_col)
-                    count += 1
-            desired_col.clear()
+                # record current row to previous to next iteration
+                previous = row
+                previous_matchID = previous[0]
+            # Since our algorithm is kind of dumb, here we need to append the last line manually
+
+            if row_num == 100998:
+                for i in range(0, 7):
+                    desired_col.append(previous[i])
+                desired_col.append(previous[26])
+                desired_col.append(previous[27])
+                # append the final result according to score difference
+                if int(row[11]) > 0:
+                    result_temp = 1
+                elif int(row[11]) == 0:
+                    result_temp = 0
+                else:
+                    result_temp = -1
+
+                # write the result to file
+                desired_col.append(result_temp)
+                writer.writerow(desired_col)
+
             row_num += 1
+            desired_col.clear()
+
+
+
 
     except IOError as e:
         print("I/O error({0}): {1}".format(e.errno, e.strerror))
--- Data_Processing/new_cols.py
+++ Data_Processing/new_cols.py
@@ -4,7 +4,7 @@ import csv
 import sys
 
 source = open('sample.csv', 'rt')
-dest = open('new_cols.csv', 'wt')
+dest = open('aggregation_first_30mins.csv', 'wt')
 row_num = 0
 count = 0
 score_home = 0
@@ -16,6 +16,7 @@ try:
     writer = csv.writer(dest)
     # before write to the row, we need to take the desired value from each row
     desired_col = []
+    mins = 0
     for row in reader:
         if row_num == 0:
             for value in row:
@@ -26,17 +27,19 @@ try:
             desired_col.append('RedCardA')
             writer.writerow(desired_col)
         else:
-            for value in row:
-                desired_col.append(value)
-            score_home = (int(row[10]) + int(row[11])) / 2
-            score_away = (int(row[10]) - int(row[11])) / 2
-            redcard_home = (int(row[12]) + int(row[13])) / 2
-            redcard_away = (int(row[12]) - int(row[13])) / 2
-            desired_col.append(score_home)
-            desired_col.append(score_away)
-            desired_col.append(redcard_home)
-            desired_col.append(redcard_away)
-            writer.writerow(desired_col)
+            if int(row[9]) == 30:
+                for value in row:
+                    desired_col.append(value)
+                score_home = (int(row[10]) + int(row[11])) / 2
+                score_away = (int(row[10]) - int(row[11])) / 2
+                redcard_home = (int(row[12]) + int(row[13])) / 2
+                redcard_away = (int(row[12]) - int(row[13])) / 2
+                desired_col.append(score_home)
+                desired_col.append(score_away)
+                desired_col.append(redcard_home)
+                desired_col.append(redcard_away)
+                writer.writerow(desired_col)
+
         desired_col.clear()
         row_num += 1
 
